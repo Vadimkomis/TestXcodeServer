7b5ffd23dc10d5580b977174b74cb3d4b4358a3e
Vadim Komisarchik <vadim@myclok.com>
2017-03-31 15:35:11 -0400

    Testing

--- TestXcodeServerTests/TestXcodeServerTests.swift
+++ TestXcodeServerTests/TestXcodeServerTests.swift
@@ -16,6 +16,10 @@ class TestXcodeServerTests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
+    func testXcodeServer() {
+        XCTAssertEqual(2, 2)
+    }
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
